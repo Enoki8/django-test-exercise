8ea291640deb77c3b3eb8eba1f3c16f1f7c22207
村井蓮 <s1f102402989@iniad.org>
2025-07-10 13:03:32 +0900

    Update views.py
    flake8でのエラー内容の修正

--- todo/views.py
+++ todo/views.py
@@ -41,6 +41,10 @@ def delete(request, task_id):     # 削除処理を追加
     return redirect(index)
 
 def update(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
